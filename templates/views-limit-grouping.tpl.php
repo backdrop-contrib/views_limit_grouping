ee3a7d1538a68b2bb2b22a8ba9b07196100199a7
laryn <laryn@cedc.org>
2017-06-29 15:54:58 -0500

    Allow row classes to be used

--- templates/views-limit-grouping.tpl.php
+++ templates/views-limit-grouping.tpl.php
@@ -2,15 +2,23 @@
 
 /**
  * @file views-limit-grouping.tpl.php
- * Basically, just a copy of views-view-unformatted.tpl.php.
+ *
+ * - $title: The title of this group of rows.  May be empty.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are indexed by row number.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ *
+ * @ingroup views_templates
  */
 ?>
+
 <div class="views-limit-grouping-group">
   <?php if (!empty($title)): ?>
     <?php print $title; ?>
   <?php endif; ?>
   <?php foreach ($rows as $row_count => $row): ?>
-    <div class="views-row views-row-<?php print $zebra; ?> <?php print $classes; ?>">
+  <div <?php if (!empty($row_classes[$row_count])) { print 'class="' . implode(' ', $row_classes[$row_count]) . '"';  } ?>>
       <?php print $row; ?>
     </div>
   <?php endforeach; ?>
